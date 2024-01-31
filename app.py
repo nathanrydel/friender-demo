a5c1f8621135c478fd688524b2bbe88c1c37f6a2
Nathan Rydel <nathanrydel@gmail.com>
2024-01-31 16:09:46 -0700

    updated imports

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from forms import (
     CSRFProtection, UserAddForm, UserEditForm, LoginForm,
 )
 from models import (
-    db, connect_db, User, UserHobbies, UserInterest, UserPhotos)
+    db, connect_db, User, UserHobbies, UserInterest) #, UserPhotos)
 
 load_dotenv()
 
