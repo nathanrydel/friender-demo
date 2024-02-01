314dbfe29d931a891efc4e8627ea1f22492bb306
Nathan Rydel <nathanrydel@gmail.com>
2024-01-31 17:20:08 -0700

    turn off FDT redirects and SQLA echo

--- app.py
+++ app.py
@@ -20,8 +20,8 @@ CURR_USER_KEY = "curr_user"
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-app.config['SQLALCHEMY_ECHO'] = True
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+app.config['SQLALCHEMY_ECHO'] = False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 toolbar = DebugToolbarExtension(app)
 
