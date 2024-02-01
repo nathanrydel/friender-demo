5f6e5386fd7ac9ec2b6e5bff51c5517670668a42
Nathan Rydel <nathanrydel@gmail.com>
2024-01-31 18:45:33 -0700

    added more interests to seed

--- seed.py
+++ seed.py
@@ -1,27 +1,43 @@
 """Create initial database tables"""
 
 from app import db
-from models import Hobbies, Interests
+from models import User, Hobby, Interest
 
 db.drop_all()
 db.create_all()
 
 db.session.commit()
 
+
+#######################################
+# add users
+# all users have password of password
+
+u1 = User.signup("test", "test@example.com", "password",
+                 "94304", "8005551234", "test_fname", "test_lname")
+
+u2 = User.signup("mctest2", "test2@example.com", "password",
+                 "94022", "8005552345", "mctest_fname", "mctest_lname")
+
+db.session.add_all([u1, u2])
+db.session.commit()
+
 #######################################
 # add interests
 
-basketball = Interests(code="bball", name="Basketball")
-golf = Interests(code="golf", name="Golf")
+basketball = Interest(code="basketball", name="Basketball")
+golf = Interest(code="golf", name="Golf")
+rockclimbing = Interest(code="rock-climbing", name="Rock Climbing")
+skiing = Interest(code="skiing", name="Skiing")
 
-db.session.add_all([basketball, golf])
+db.session.add_all([basketball, golf, rockclimbing, skiing])
 db.session.commit()
 
 #######################################
 # add hobbies
 
-tabletop= Hobbies(code="tabletop", name="tabletop")
-skydiving= Hobbies(code="skydiving", name="skydiving")
+tabletop = Hobby(code="table-top-rpgs", name="Table Top RPGs")
+skydiving = Hobby(code="sky-diving", name="Sky Diving")
 
 db.session.add_all([tabletop, skydiving])
 db.session.commit()
