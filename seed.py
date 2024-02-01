be4fc17e7b771bbd358e604cc6daa7e692b79664
Charles Chitty <charlescchitty@gmail.com>
2024-01-31 17:08:35 -0800

    updated seed file to include pre-set hobbies and interests

--- seed.py
+++ seed.py
@@ -1,8 +1,27 @@
 """Create initial database tables"""
 
 from app import db
+from models import Hobbies, Interests
 
 db.drop_all()
 db.create_all()
 
+db.session.commit()
+
+#######################################
+# add interests
+
+basketball = Interests(code="bball", name="Basketball")
+golf = Interests(code="golf", name="Golf")
+
+db.session.add_all([basketball, golf])
+db.session.commit()
+
+#######################################
+# add hobbies
+
+tabletop= Hobbies(code="tabletop", name="tabletop")
+skydiving= Hobbies(code="skydiving", name="skydiving")
+
+db.session.add_all([tabletop, skydiving])
 db.session.commit()
