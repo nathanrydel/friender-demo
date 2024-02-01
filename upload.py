e1abd741eb25b99a54e8c2dbfbfcc435d3765c1d
Charles Chitty <charlescchitty@gmail.com>
2024-02-01 10:35:30 -0800

    added upload.py file which holds helper functions for aws s3 file uploads

---
+++ upload.py
@@ -0,0 +1,35 @@
+import logging
+import boto3
+from botocore.exceptions import ClientError
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+S3_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
+ACCESS_KEY_ID = os.environ["ACCESS_KEY_ID"]
+SECRET_ACCESS_KEY = os.environ["SECRET_ACCESS_KEY"]
+
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    # Upload the file
+    s3_client = boto3.client('s3',
+                             aws_access_key_id=ACCESS_KEY_ID,
+                             aws_secret_access_key=SECRET_ACCESS_KEY)
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
