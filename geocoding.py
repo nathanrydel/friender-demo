b86ce1477be61c6ab1dbc8165601143430828030
Charles Chitty <charlescchitty@gmail.com>
2024-02-01 20:59:11 -0800

    Fixed bug where location object was passed instead of latitude and longitude coordinates

--- geocoding.py
+++ geocoding.py
@@ -1,5 +1,5 @@
 from geopy.geocoders import Nominatim
-from geopy import distance
+from geopy.distance import geodesic
 
 def find_coordinate(zipcode, country="USA"):
     """Receives an input of a zipcode and an optional country input
@@ -8,8 +8,12 @@ def find_coordinate(zipcode, country="USA"):
     geolocator = Nominatim(user_agent="friender")
     location = geolocator.geocode(f"{zipcode}, {country}")
     # print(geolocator.reverse(f"{location.latitude}, {location.longitude}"))
-    return location
+    if location is not None:
+        return location
+    else:
+        print(f"Location not found for zipcode: {zipcode}")
 
+#TODO: Handle non-zipcodes? in route?
 
 def is_within_radius(location, second_location, radius):
     """Receives two location objects of longitude and
@@ -17,10 +21,13 @@ def is_within_radius(location, second_location, radius):
     depending on whether the distance between the locations is within
     the allowed radius. """
 
-    return (distance.distance(location, second_location.miles) <= radius)
+    return (geodesic((location.latitude, location.longitude),
+                     (second_location.latitude, second_location.longitude))
+                     .miles <= radius)
 
 
 #Only accepts USA zipcodes
+#TODO: Potentially store longitude and latitude?  Avoid repeated geocoding requests
 def find_nearby_users(user, other_users, radius):
     """Receives an input of a user object, an array of user objects, and a
     radius. Returns an array of user objects if those users' zipcodes are within
@@ -43,3 +50,9 @@ def find_nearby_users(user, other_users, radius):
 #BUG STORY
 #**** 1254, ဒဂုံဆိပ်ကမ်း, Yangon East, Yangon, ရန်ကုန်တိုင်းဒေသကြီး, မြန်မာ
 #**** Свобода, Батівська селищна громада, Берегівський район, 90210, Закарпатська область, Україна
+    user_location = (location.latitude, location.longitude)
+
+    for other in users:
+        other_location = find_coordinate(other.zipcode)
+        if distance.distance(user_location, other_location).miles <= distance:
+            nearby_users.append(other)
