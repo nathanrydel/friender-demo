52ab36729795cf66099a5f00fcce60fe6c616504
Charles Chitty <charlescchitty@gmail.com>
2024-02-02 11:17:29 -0800

    updated geocoding to include longitude and latitude from User class

--- geocoding.py
+++ geocoding.py
@@ -1,19 +1,18 @@
 from geopy.geocoders import Nominatim
 from geopy.distance import geodesic
 
+#Only accepts USA zipcodes
 def find_coordinate(zipcode, country="USA"):
     """Receives an input of a zipcode and an optional country input
     and returns the longitude and latitude of that zipcode"""
 
     geolocator = Nominatim(user_agent="friender")
     location = geolocator.geocode(f"{zipcode}, {country}")
-    # print(geolocator.reverse(f"{location.latitude}, {location.longitude}"))
     if location is not None:
         return location
     else:
-        print(f"Location not found for zipcode: {zipcode}")
+        return None
 
-#TODO: Handle non-zipcodes? in route?
 
 def is_within_radius(location, second_location, radius):
     """Receives two location objects of longitude and
@@ -26,23 +25,23 @@ def is_within_radius(location, second_location, radius):
                      .miles <= radius)
 
 
-#Only accepts USA zipcodes
-#TODO: Potentially store longitude and latitude?  Avoid repeated geocoding requests
 def find_nearby_users(user, other_users, radius):
     """Receives an input of a user object, an array of user objects, and a
     radius. Returns an array of user objects if those users' zipcodes are within
     the allowed radius of the initial user object."""
 
-    # users = User.query.filter(User.username != user.username).all()
-
     nearby_users = []
 
-    user_location = find_coordinate(user.zipcode)
+    user_location = {user.latitude, user.longitude}
+
+    if user.latitude == None or user.longitude == None:
+        raise ValueError("Invalid Zipcode")
 
     for other_user in other_users:
-        other_user_location = find_coordinate(other_user.zipcode)
-        if is_within_radius(user_location, other_user_location, radius):
-            nearby_users.append(other_user)
+        if other_user.latitude and other_user.longitude:
+            other_user_location = {other_user.latitude, other_user.longitude}
+            if is_within_radius(user_location, other_user_location, radius):
+                nearby_users.append(other_user)
 
     return nearby_users
 
